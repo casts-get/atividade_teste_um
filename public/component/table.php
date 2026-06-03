cacfb4f23aa084a49a58fc52cd7bb8c63baf1991
casts-get <Carine_dos-santos@estudante.sesisenai.org.br>
2026-06-03 08:11:40 -0300

    doc- adicionei os comentarios da pagina table

--- public/component/table.php
+++ public/component/table.php
@@ -1,30 +1,38 @@
 <hr>
+<!-- Linha horizontal para separar o conteúdo da página -->
 
 <h4> Usuários Cadastrados</h4>
 
 <table border="1" cellpadding="2">
-
+<!-- Tabela que exibirá os usuários cadastrados -->
     <tr>
         <th>ID</th>
         <th>Usuário</th>
         <th>Senha</th>
     </tr>
+    <!-- Cabeçalho da tabela -->
 
     <?php
     
 
     $sqlUsuario = "SELECT * FROM usuario";
+     // Consulta SQL para buscar todos os usuários da tabela "usuario"
 
     $resultadoUsuarios = $conn -> query($sqlUsuario);
+     // Executa a consulta no banco de dados
+
 
     while($linha = $resultadoUsuarios->fetch_assoc()){
+        // Percorre todos os registros retornados pela consulta
         echo "<tr>
         
             <td>" . $linha["id"] . "</td>
             <td>" . $linha["usuario"] . "</td>
             <td>" . $linha["senha"] . "</td>
+            
         
         </tr>";
+         // Cria uma linha da tabela para cada usuário encontrado
     }
     
     ?>
