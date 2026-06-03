6f4de23493ee1df3e1aa499b69baecb32e6e4897
casts-get <Carine_dos-santos@estudante.sesisenai.org.br>
2026-06-03 08:36:01 -0300

    doc- comentarios da pagina script

--- infra/db/script.sql
+++ infra/db/script.sql
@@ -1,11 +1,20 @@
 CREATE DATABASE sistema_simples;
+-- Cria o banco de dados chamado sistema_simples
 
 USE sistema_simples;
+-- Seleciona o banco de dados para uso
+
 
 CREATE TABLE usuario (
+    -- Cria a tabela de usuários
     id INT AUTO_INCREMENT PRIMARY KEY,
+     -- Campo ID com incremento automático e chave primária
     usuario VARCHAR(255) NOT NULL,
+     -- Campo para armazenar o nome de usuário
     senha VARCHAR(255) NOT NULL
+     -- Campo para armazenar a senha
+
 );
 
 INSERT INTO usuario (usuario, senha) VALUE ('admin','123');
+-- Insere um usuário administrador padrão na tabela
