c5b2aaf1daf236d247a773b34724588774f1a918
casts-get <Carine_dos-santos@estudante.sesisenai.org.br>
2026-06-03 08:32:35 -0300

    doc-adicionei comentarios da pagina connect

--- infra/db/connect.php
+++ infra/db/connect.php
@@ -1,12 +1,15 @@
 <?php
     session_start();
+      // Inicia a sessão para permitir o uso de variáveis de sessão
 
     $host = "localhost";
     $user = "root";
     $pass = "root";
     $db = "sistema_simples";
+    // Dados necessários para conectar ao banco de dados
     
     $conn = new mysqli($host,$user,$pass,$db);
+     // Cria a conexão com o banco utilizando a classe mysqli
 
     // if($conn->connect_error){
     //     die("Erro na conexão");
