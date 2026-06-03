7d81f3b541cf60547d68c14566dd58d557f8f82b
casts-get <Carine_dos-santos@estudante.sesisenai.org.br>
2026-06-03 08:22:59 -0300

    doc- adicionei comentarios a pagina logout

--- public/logout.php
+++ public/logout.php
@@ -1,6 +1,12 @@
 <?php
+
     session_start();
+     // Inicia a sessão para ter acesso aos dados armazenados nela
     session_destroy();
+     // Remove todos os dados da sessão atual
     header("Location: ../index.php");
+     // Redireciona o usuário para a página de login
     exit();
+    // Encerra a execução do script para garantir que nada mais seja executado
+
 ?>
